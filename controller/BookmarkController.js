7de2faee4363b81e489efbca811d9533675669c7
Amol Chitale <amolchitaledcjr@gmail.com>
2024-09-23 22:19:42 +0530

    add bookmark done

--- controller/BookmarkController.js
+++ controller/BookmarkController.js
@@ -1,14 +1,41 @@
 const User = require('../model/User.js')
 const Story = require('../model/Story')
 
-const addBookmark = (req,res)=>{
-
+const addBookmark = async (req, res) => {
+    try {
+        const { story_id, user_id } = req.body
+    
+        const storyExists = await Story.exists({ _id: story_id })
+        if (!storyExists) {
+            return res.status(404).json({
+                success: false,
+                error:'Story_Not_Found',
+                message: 'Story not found'
+            })
+        }
+    
+        const updatedUser = await User.findByIdAndUpdate(user_id, {$addToSet: { bookmarks: story_id }}, { new: true, runValidators: true });
+    
+        if(!updatedUser){
+            return res.status(400).json({
+                success:false,
+                error:'User_Not_Found',
+                message:'User not found'
+            })
+        }
+    
+        return res.status(200).json({
+            success:true,
+            payload:updatedUser
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            error,
+            message:'Internal Server Error'
+        })
+    }
 }
-
-const getBookmarks = (req,res)=>{
-
-}
-
 
 module.exports = {
     addBookmark,
--- controller/FeedController.js
+++
@@ -1,9 +0,0 @@
-const Story = require('../model/Story.js')
-
-const getFeed = (req,res)=>{
-
-}
-
-const filterFeed = (req,res)=>{
-    
-}
